f798217c2fea858378826635fd3beb4a5c45f8ae
berkayakkocc <77438994+berkayakkocc@users.noreply.github.com>
2022-08-23 09:51:22 +0300

    Middleware düzenlendi.

--- src/demoProjects/rentACar/WebAPI/Program.cs
+++ src/demoProjects/rentACar/WebAPI/Program.cs
@@ -25,8 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.ConfigureCustomExceptionMiddleware();
-
+if (app.Environment.IsProduction())
+    app.ConfigureCustomExceptionMiddleware();
 app.UseAuthorization();
 
 app.MapControllers();
